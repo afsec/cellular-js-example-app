2d5d43e4e5c05bea2ae1f9a17a23c43d7f520de3
Antonio Feitosa <antonio.cfc@gmail.com>
2020-03-09 17:09:11 -0300

    AddEventListener to Buttons comments added

--- src/js/states/showuserspage/presenter.js
+++ src/js/states/showuserspage/presenter.js
@@ -10,12 +10,14 @@ const stateShowUsersPagePresenter = async () => {
 
     waitForElement({ "selector": "tr#last-row" }, () => {
 
+        // * Button: Edit
         document.querySelectorAll('td.editable-cell').forEach((tdEditable) => {
             tdEditable.childNodes[3].addEventListener('click', () => {
                 stateShowUsersPageShowModal(tdEditable)
             })
         })
 
+        // * Button: Delete
         document.querySelectorAll('td[role="delete"]').forEach((tdDelete) => {
             tdDelete.childNodes[1].addEventListener('click', () => {
                 stateShowUsersPageShowModal(tdDelete)
